1473d8440e4f1df4b3737070426e899d7c6dc727
Robert Gravina <robert.gravina@gmail.com>
2017-05-08 23:30:35 +1000

    Show supplier books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,5 +5,6 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @supplier_books = SupplierBook.where(book: @book)
   end
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,6 @@
 class Supplier < ApplicationRecord
+  has_many :supplier_books
+  has_many :books, through: :supplier_books
+  has_many :supplier_discounts
+  has_many :discounts, through: :supplier_discounts
 end
